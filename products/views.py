4bbebe7a42b161124a835c0f9284d3da6961f180
HollyT85 <h.e.turner@gmail.com>
2022-06-01 11:41:17 +0000

    basic product rendering & added urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Products
 
-# Create your views here.
+
+def products(request):
+    """
+    Products page view
+    """
+    products = Products.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
