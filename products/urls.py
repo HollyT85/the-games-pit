18f9de6a612ef2a503be32b56f23abebb2c53425
HollyT85 <h.e.turner@gmail.com>
2022-06-02 14:06:32 +0000

    product id not working

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name="products"),
-    path('<product_id>', views.product_info, name='product_info'),
+    path('<product_id>', views.product_info, name='product-info'),
 ]
