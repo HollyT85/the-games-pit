2c319757b4cb46dc8a775af5b8d875f7ed579d2a
HollyT85 <h.e.turner@gmail.com>
2022-06-10 10:12:32 +0000

    added to db

--- the_games_pit/settings.py
+++ the_games_pit/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-FREE_DELIVERY_THRESHOLD = £50
+FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY = 10
 
 # Default primary key field type
