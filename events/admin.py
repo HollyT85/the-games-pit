e04a55638c63d9c55a9975440590a74ff7f27e3c
HollyT85 <h.e.turner@gmail.com>
2022-06-16 09:47:02 +0000

    event admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Events
 
-# Register your models here.
+
+class EventsAdmin(admin.ModelAdmin):
+    """
+    Display of event categories
+    """
+    list_display = (
+        'name',
+        'description',
+        'date',
+        'time',
+    )
--- events/views.py
+++ events/views.py
@@ -1,3 +1,16 @@
+"""
+imports for functionality
+"""
 from django.shortcuts import render
+from .models import Events
 
-# Create your views here.
+
+def all_events(request):
+    """
+    retrieve all events 
+    """
+    events = Events.objects.all()
+    context = {
+        'events': events,
+    }
+    return render(request, 'events/events.html', context)
