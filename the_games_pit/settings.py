cff94d7b2fd5a1dbe3dfb2da07fdf8c6ccec7e2a
HollyT85 <h.e.turner@gmail.com>
2022-06-16 19:15:48 +0000

    secret key added to heroku

--- the_games_pit/settings.py
+++ the_games_pit/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ul$rjo*+6sxx0m^6zd(h$7a0#oa*b8#f3)&aps2w)+znx4+pv9'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-games-pit.herokuapp.com', 'localhost']
 
