fd1676390195930dd99c39506c814e13feeef8f1
HollyT85 <h.e.turner@gmail.com>
2022-06-17 18:17:04 +0000

    spelling error on bucket name

--- the_games_pit/settings.py
+++ the_games_pit/settings.py
@@ -168,7 +168,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # config bucket on AWS
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'the_games_pit'
+    AWS_STORAGE_BUCKET_NAME = 'the-games-pit'
     AWS_S3_REGION_NAME = "eu-west-2"
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
