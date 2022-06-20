12fde1426b439803802bf976a618162684d4eff9
HollyT85 <h.e.turner@gmail.com>
2022-06-20 16:56:38 +0000

    item total working for full price and offer price

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -2,10 +2,11 @@ from django import template
 
 register = template.Library()
 
-
+@register.filter(name='item_total')
 def item_total(price, quantity):
     return price * quantity
 
 
-def offer_price(offer_price, quantity):
+@register.filter(name='offer_total')
+def offer_total(offer_price, quantity):
     return offer_price * quantity
