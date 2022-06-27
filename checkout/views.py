4b263ec36bd2e6767f7fc6ac5f660c2ff802479b
HollyT85 <h.e.turner@gmail.com>
2022-06-27 09:45:33 +0000

    working on stripe

--- checkout/views.py
+++ checkout/views.py
@@ -3,25 +3,44 @@ imports for functionality
 """
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
+from bag.contexts import bag_contents
 from .forms import OrderForm
+
+import stripe
 
 
 def checkout(request):
     """
     checkout view
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'Bag is empty')
         return redirect(reverse('home'))
 
+    current_order = bag_contents(request)
+    total = current_order['grand_total']
+    stripe_total = round(total*100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'No public key for stripe')
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51L0s6THIk7RsIgl85O7yeckSCRu2e2VieQfhR3OemmKelvWgxOv4qDsjr8SJplXhtoHcrBNlcPptuHNndh1OZBaH00aAUtyXvn',
-        'stripe_secret_key': 'test'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret
     }
 
     return render(request, template, context)
--- static/checkout/js/stripe.js
+++ static/checkout/js/stripe.js
@@ -4,7 +4,7 @@
 */
 
 var stripe_public_key = $('#stripe_public_key_id').text().slice(1, -1);
-var stripe_secret_key = $('#stripe_secret_key_id').text().slice(1, -1);
+var client_secret = $('#client_secret_id').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var card = elements.create('card', {style: style});
@@ -21,7 +21,25 @@ var style = {
     invalid: {
         color: '#dc3545',
         iconColor: '#dc3545'
-    }
+    },
 };
 
 card.mount('#card-element');
+
+// errors 
+
+card.addEventListener('change', function (event) {
+    var cardErrors = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span role="alert">
+                <i class="fas fa-bug"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(cardErrors).html(html);
+    } else {
+        cardErrors.textContent = '';
+    }
+});
+
