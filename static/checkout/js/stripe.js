273d3174db5f5519bf3a70b3171763e247a3aeac
HollyT85 <h.e.turner@gmail.com>
2022-06-27 09:58:00 +0000

    further stripe set up

--- static/checkout/js/stripe.js
+++ static/checkout/js/stripe.js
@@ -3,9 +3,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#stripe_public_key_id').text().slice(1, -1);
-var client_secret = $('#client_secret_id').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#stripe_public_key_id').text().slice(1, -1);
+var clientSecret = $('#client_secret_id').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var card = elements.create('card', {style: style});
 var style = {
@@ -43,3 +43,32 @@ card.addEventListener('change', function (event) {
     }
 });
 
+// Form submit
+var form = document.getElementById('payment');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var cardErrors = document.getElementById('card-errors');
+            var html = `
+                <span role="alert">
+                    <i class="fas fa-bug"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(cardErrors).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
