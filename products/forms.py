281ac9d8ba00fe0a13b142f87c399d90c32ed7b8
HollyT85 <h.e.turner@gmail.com>
2022-07-09 18:01:18 +0000

    fixed log in issues (dupe email in db) and fixed form

--- products/forms.py
+++ products/forms.py
@@ -16,10 +16,9 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        sub_cats = SubCat.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-        self.fields['category'].choices = categories
-        self.fields['sub_cat'].choices = sub_cats
+        self.fields['category'].choices = friendly_names
 
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = ''
