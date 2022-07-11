708b5cdf6bf1a98ff4442e4173b138ca31c5fcf6
HollyT85 <h.e.turner@gmail.com>
2022-07-11 08:50:16 +0000

    removed whitenoise

--- the_games_pit/settings.py
+++ the_games_pit/settings.py
@@ -63,8 +63,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'the_games_pit.urls'
