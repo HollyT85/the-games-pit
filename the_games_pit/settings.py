0766a1411b507719e0afb5322a884a58759b372f
HollyT85 <h.e.turner@gmail.com>
2022-07-11 17:55:41 +0000

    deployment

--- the_games_pit/settings.py
+++ the_games_pit/settings.py
@@ -124,7 +124,7 @@ WSGI_APPLICATION = 'the_games_pit.wsgi.application'
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse('os.environ('DATABASE_URL')')
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
