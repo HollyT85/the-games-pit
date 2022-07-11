7df63a86e8567658337a4e78a161c258a26b55d8
HollyT85 <h.e.turner@gmail.com>
2022-07-11 08:38:29 +0000

    attempting redeployment as it broke

--- the_games_pit/settings.py
+++ the_games_pit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,6 +130,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
